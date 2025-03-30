dd01d14a2ed891a66d8df671d172c725210f65f3
mslee98 <root@leeminsung-ui-MacBookPro.local>
2025-03-31 02:04:12 +0900

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,11 +33,13 @@ export default function RootLayout({
       <body className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
           <main className="min-h-screen flex flex-col items-center">
+
+
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
@@ -50,6 +52,10 @@ export default function RootLayout({
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
               </nav>
+
+
+
+
               <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
               </div>
